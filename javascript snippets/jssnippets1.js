3fd7b80c8fbe4abde27f248145c0784dd6716b00
grevillopes <grevillopes@gmail.com>
2017-05-29 11:36:26 -0500

    js snippet for getting first and last name from text field

--- javascript snippets/jssnippets1.js
+++ javascript snippets/jssnippets1.js
@@ -1,3 +1,12 @@
 /**
  * Created by grevi on 5/29/2017.
  */
+function input() {
+    var name = document.getElementById("name").value;
+    var results = document.getElementById("results");
+    var First = name.substring(0, name.indexOf(" "));
+    var Last = name.substring(name.lastIndexOf(" ")+1, name.length);
+    console.log(First);
+    results.innerHTML = "FirstName is <b>" + First + "</b>";
+    results.innerHTML += "<br> LastName is <b>" + Last + "</b>";
+}
